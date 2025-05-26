4cabb9ad3a8577da911ad0b2f7cd11916dc5d0d4
Kpeca <aleksandar.kresojevic@gmail.com>
2025-05-26 23:28:58 +0200

    promena hero sekcije

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,11 +1,17 @@
 import HeroImage2 from "../assets/img/hero.png";
 
 const Hero = () => {
+  const handleWhatsAppClick = () => {
+    window.open("https://wa.me/381645811258", "_blank");
+  };
   return (
-    <section style={{ backgroundColor: "#ded0b7" }}>
+    <section className="pt-8" style={{ backgroundColor: "#ded0b7" }}>
       <div className="container mx-auto flex flex-col  items-center justify-between gap-6 pt-16 md:pt-20 pb-10">
         <img className="bg-cover w-full h-full" src={HeroImage2} />
-        <button className="bg-black text-white text-lg font-serif px-6 py-3 rounded-md cursor-pointer">
+        <button
+          onClick={handleWhatsAppClick}
+          className="bg-black text-white text-lg font-serif px-4 py-2 md:px-8 md:py-3 rounded-md cursor-pointer"
+        >
           Poruči svoj set
         </button>
       </div>
